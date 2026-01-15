0e9404050f27389327584cd12e336c83858763d0
石狮子 <ssz2048@163.com>
2026-01-15 18:11:11 +0800

    init

---
+++ jifenWatch Watch App/jifenWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  jifenWatchApp.swift
+//  jifenWatch Watch App
+//
+//  Created by Yangyang Shi on 2026/1/15.
+//
+
+import SwiftUI
+
+@main
+struct jifenWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
