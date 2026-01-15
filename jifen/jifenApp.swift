a4362839566366f0cfbae0b4b3308fe0846d9b5d
石狮子 <ssz2048@163.com>
2026-01-15 18:07:42 +0800

    Initial Commit

---
+++ jifen/jifenApp.swift
@@ -0,0 +1,17 @@
+//
+//  jifenApp.swift
+//  jifen
+//
+//  Created by Yangyang Shi on 2026/1/15.
+//
+
+import SwiftUI
+
+@main
+struct jifenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
